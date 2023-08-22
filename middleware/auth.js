fcffcbf78ad18d9a5344f22ccdd47a480bd5290c
Umair Pervaiz Butt <umair.pervaiz@developersstudios-MacBook-Air-92.local>
2023-08-22 17:35:33 +0500

    Added auth middleware and removed unwanted code

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,14 @@
-import { Jwt } from "jsonwebtoken";
+import Jwt  from "jsonwebtoken";
 
 export const authenticate = async (req, res, next) => {
     const {token} = req.query;
+    console.log(token);
+
+    if(!token){
+        res.status(401).json({
+            error: 'error',
+            msg: "Unauthorized"
+        })
+    }
+    next();
 }
